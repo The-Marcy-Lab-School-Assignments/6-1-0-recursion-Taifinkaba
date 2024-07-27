00ac279890177b15188d924f5c62a6a330d035bc
Taifinkaba <taifinkabad@gmail.com>
2024-07-26 20:08:24 -0400

    from-scratch complete

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -13,12 +13,42 @@ const reverse = (str) => {
 };
 
 // Iterative approach to solving the fibonacci problem
-const fibIter = (n) => {};
+const fibIter = (n) => {
+    if (n <= 0) return 0;
+    if (n === 1) return 1;
+    
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
+};
 
 // Recursive fibonacci
-const fibRec = (n) => {};
+const fibRec = (n) => {
+    if (n <= 0) return 0;
+    if (n === 1) return 1;
+    
+    return fibRec(n - 1) + fibRec(n - 2);
+};
 
 // Return the index of target in arr, or -1 if not found using recursion 
-const binarySearch = (arr, target, start = 0, end = arr.length - 1) => {};
+const binarySearch = (arr, target, start = 0, end = arr.length - 1) => {
+    // Base case
+    if (start > end) return -1; 
+    
+    // middle index
+    const mid = Math.floor((start + end) / 2); 
+
+    if (arr[mid] === target) {
+        return mid; 
+    } else if (arr[mid] > target) {
+        return binarySearch(arr, target, start, mid - 1);
+    } else {
+        return binarySearch(arr, target, mid + 1, end);
+    }
+};
 
 module.exports = { sum, reverse, fibRec, fibIter, binarySearch };
