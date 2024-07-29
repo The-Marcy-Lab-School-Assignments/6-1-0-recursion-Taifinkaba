6c1e82803dae7502e17d2d0a540f39c76b70b061
Taifinkaba <taifinkabad@gmail.com>
2024-07-29 15:55:33 -0400

    stuff

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -3,8 +3,11 @@
 // Calculate and return the total sum using recursive approach
 const sum = (arr, total = 0, idx = 0) => {
     if(idx === arr.length) return total;
-    return sum(arr, total + arr[idx],idx + 1);
+    return sum(arr,total+arr[idx],idx+1);
 }
+
+
+
 
 // Reverse string using recursive approach
 const reverse = (str) => {
@@ -44,7 +47,8 @@ const binarySearch = (arr, target, start = 0, end = arr.length - 1) => {
 
     if (arr[mid] === target) {
         return mid; 
-    } else if (arr[mid] > target) {
+    } 
+    if (arr[mid] > target) {
         return binarySearch(arr, target, start, mid - 1);
     } else {
         return binarySearch(arr, target, mid + 1, end);
